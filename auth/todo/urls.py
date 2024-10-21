099bda429bc54bccde09ca8155c54196af0c583a
Emu12-tile <emuaniley@gmail.com>
2024-10-21 23:46:53 +0300

    hello

--- auth/todo/urls.py
+++ auth/todo/urls.py
@@ -7,6 +7,7 @@ from .views import  TodoListCreateView,TodoUpdateView,TodoDeleteView
 # router = DefaultRouter()
 urlpatterns = [
     path("", TodoListCreateView.as_view(), name="register"),
+    path("/register", TodoListCreateView.as_view(), name="register"),
     path('delete/<int:pk>/', TodoDeleteView.as_view(), name='todo-delete'), 
     path('update/<int:pk>/', TodoUpdateView.as_view(), name='todo-update'), 
 
--- auth/todo/views.py
+++ auth/todo/views.py
@@ -13,6 +13,17 @@ class TodoUpdateView(generics.RetrieveUpdateAPIView):
     partial = True
     
     
+# class TodoDeleteView(generics.DestroyAPIView):
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
+
+#     def destroy(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         self.perform_destroy(instance)
+#         return Response({"message": "Todo deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+
+#     def perform_destroy(self, instance):
+#         instance.delete()
 class TodoDeleteView(generics.DestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
@@ -24,4 +35,3 @@ class TodoDeleteView(generics.DestroyAPIView):
 
     def perform_destroy(self, instance):
         instance.delete()
-
